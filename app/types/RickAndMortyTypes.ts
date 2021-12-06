17571e4d0c8291ba7a6049774285f90d0908f75c
waligustav <wgbjork@gmail.com>
2021-12-06 20:43:07 +0100

    penis

--- app/types/RickAndMortyTypes.ts
+++ app/types/RickAndMortyTypes.ts
@@ -2,19 +2,61 @@ export type Character = {
   id: number;
   name: string;
   species?: string;
+  gender: string;
+  status: string;
   origin?: {
     name: string;
     url: string;
   };
   image: string;
   };
+
+  export type AllCharacters = {
+    info: {
+      next: string | null;
+      prev: string | null;
+    };
+    results: [
+      {
+        id: number;
+        name: string;
+        species?: string;
+        origin?: {
+          name: string;
+          url: string;
+        };
+        image: string;
+      }
+    ];
+  };
   
   export type Location = {
     id: number;
     name: string;
+    type?: string;
+    dimension?: string;
+    residents: string[];
   };
   
-  export type Episode = {
+  export type AllLocations = {
+    info: {
+      pages: number;
+    };
+    results: [
+      {
+        id: number;
+        name: string;
+        type: string;
+        dimension: string;
+        residents: string[];
+      }
+    ];
+  };
+  
+  export type Episodes = {
     id: number;
     name: string;
+    episode: string;
+    characters?: string[];
   };
+  
